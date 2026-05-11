3860fb57c7178d23aabf480eca0e3b62190c9d52
Rodriramos <alu0101479958>
2026-05-11 18:16:03 +0100

    Backend structured

---
+++ backend/src/test/java/com/realtimechat/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtimechat.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
